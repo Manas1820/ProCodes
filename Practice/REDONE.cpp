a2c2af0269b328f8ae1cd89bebc628be3ec7a891
Manas Gupta <53006261+Manas1820@users.noreply.github.com>
2020-07-17 00:58:21 +0530

    Add file via upload

---
+++ Practice/REDONE.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define lo(n) for(ll int i=1;i<n;i++)
+#define fo(k,n) for(int i=k;i<n;i++)
+
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+#define pb push_back
+#define mp make_pair
+
+#define F first
+#define S second
+
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+
+
+int main() {
+    fastio();
+    ll int t;
+    sl(t);
+    ll int n;
+    ll int arr[1000000];
+    arr[0]=1;
+    lo(1000000) arr[i]=(arr[i-1]+(i+1)+arr[i-1]*(i+1))%1000000007; 
+	while(t--){
+	     sl(n);
+	     cout<<arr[n-1]<<endl;
+	}
+	        
+	return 0;
+}
