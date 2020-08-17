94590a7ce84959296e0ceb5f3c0a1a09a053f345
Manas Gupta <53006261+Manas1820@users.noreply.github.com>
2020-08-17 19:18:06 +0530

    Create LINCHESS.cpp

---
+++ August Challenge 2020 Division 2/LINCHESS.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define lo(n) for(int i=0;i<n;i++)
+#define fo(k,n) for(int i=k;i<n;i++)
+
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+#define pb push_back
+#define mp make_pair
+
+#define F first
+#define S second
+
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+
+void solve(){
+   ll int n,k;
+   sl(n);
+   sl(k);
+   ll int arr[n],min_num=-1;
+   lo(n) sl(arr[i]);
+   lo(n){
+      if(arr[i]>=k) continue;
+      if(k % arr[i]== 0 && arr[i]>min_num) min_num=arr[i];
+   }
+   cout<<min_num<<endl;
+}
+
+int main() {
+    ll int t;
+    sl(t);
+	while(t--)
+	    solve();
+	return 0;
+}
