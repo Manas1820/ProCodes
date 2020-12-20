91b82feba4677b7962cfb2eab8a8ee3963c8fac2
Manas Gupta <53006261+Manas1820@users.noreply.github.com>
2020-12-21 02:00:14 +0530

    Create  UWCOI21B.cpp

---
+++ UWCOI 2021 (Rated for all)/UWCOI21B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define lo(n) for(int i=0;i<n;i++)
+#define fo(k,n) for(int i=k;i<n;i++)
+
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+#define pb push_back
+#define mp make_pair
+
+#define F first
+#define S second
+
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+
+
+void solve(){
+    ll int n,m;
+    cin>>n>>m;
+    ll int cost=0;
+    ll int x,y;
+    vl a,b;
+    
+    lo(n) {cin>>x;a.pb(x);}
+    lo(m) {cin>>y;b.pb(y);}
+    ll int activation = *min_element(a.begin(), a.end()); 
+    sortall(b);
+    lo(m){
+        if(activation>b[i]) cost+=n;
+        else break;
+    }
+    cout<<cost<<endl;
+}
+
+int main() {
+    //fastio();
+    solve();
+	return 0;
+}
