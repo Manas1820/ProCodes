fcf2053ebe7c6facb0a13d4fdf4a674e605ee8bd
Kanit Mann <kanitmann90@gmail.com>
2021-10-07 22:28:00 +0530

    :yellow_circle: Value equal to index

--- Love Babbar DSA Sheet/102.cpp
+++ Love Babbar DSA Sheet/102.cpp
@@ -5,6 +5,11 @@ Difficulty: School
 Accuracy: 54.64% Submissions: 28097 Points: 0
 
 Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+Input: 
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+Explanation: Only Arr[2] = 2 exists here.
 
 */
 
