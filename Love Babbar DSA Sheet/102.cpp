426170b956832c40c35f2afc241b6fbf05510ace
Kanit Mann <kanitmann90@gmail.com>
2021-10-07 22:23:30 +0530

    :yellow circle: Value equal to index

---
+++ Love Babbar DSA Sheet/102.cpp
@@ -0,0 +1,72 @@
+/*
+Value equal to index value
+
+Difficulty: School 
+Accuracy: 54.64% Submissions: 28097 Points: 0
+
+Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> valueEqualToIndex(int arr[], int n)
+    {
+        int arr2[n];
+        vector<int> soln;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == arr[i - 1])
+                soln.push_back(i);
+        }
+        return soln;
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.valueEqualToIndex(arr, n);
+        if (ans.empty())
+        {
+            cout << "Not Found";
+        }
+        else
+        {
+            for (int x : ans)
+            {
+                cout << x << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+
+/*
+
+Result: 
+
+Total Time Taken:
+0.3/1.7
+
+*/
