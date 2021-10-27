0f99ed755786589f0cfb52a20d8a5be95a8bacd9
Pankaj Singh Sheoran <pankajsheoran728@gmail.com>
2021-10-27 14:05:01 -0400

    Added Leetcode solution

---
+++ LeetCode/Leetcode Problems/Single Number.cpp
@@ -0,0 +1,20 @@
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    
+}
+
+int main()
+{
+    vector<int> nm{2, 2 ,1};
+    singleNumber(nm);
+    return 0;
+}
